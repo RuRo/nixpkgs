fca66a90156a5a042a6995599ef3c77214e2ceb6
ruro <ruro.ruro@ya.ru>
2025-02-10 13:37:55 +0300

    temp

--- pkgs/development/cuda-modules/cuda-samples/generic.nix
+++ pkgs/development/cuda-modules/cuda-samples/generic.nix
@@ -160,6 +160,7 @@ backendStdenv.mkDerivation (finalAttrs: {
           # but cuda_profiler_api is only available since CUDA 11.8
           "volumeRender" # cuda_profiler_api.h: No such file or directory
         ];
+      # TODO: Report upstream? https://github.com/NVIDIA/cuda-samples/issues/264
       missingLibs = [
         # For some reason, these samples (and only these samples)
         # fail to pick up the default library path.
